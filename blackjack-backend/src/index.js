d146b5fd09ab572d891f9d49c0281d39a47d6c56
Matheus Morcinek <matheusmorcinek@gmail.com>
2024-04-04 23:08:36 -0300

    app api updates

--- blackjack-backend/src/index.js
+++ blackjack-backend/src/index.js
@@ -51,15 +51,62 @@ app.get('/blackjack/status', (req, res) => {
 });
 
 app.post('/blackjack/hit', (req, res) => {
-    blackjack.playerHit();
+    if (!blackjack) {
+        return res.status(400).json({
+            message: "Game not found. Please start a new game."
+        });
+    };
 
-    res.status(200).json({
+    try {
+        blackjack.playerHit();
+
+        res.status(200).json({
+            player: {
+                hand: blackjack.playerHand,
+                score: blackjack.playerScore,
+            }
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        });
+    };
+});
+
+app.get('/blackjack/decideWinner', (req, res) => {
+    if (!blackjack) {
+        return res.status(400).json({
+            message: "Game not found. Please start a new game."
+        });
+    };
+
+    if (blackjack.status !== 'ongoing') {
+        return res.status(400).json({
+            message: "Game is already over."
+        });
+    };
+
+    blackjack.decideWinner();
+
+    const status = blackjack.status;
+    const resultMessage = status === 'player_won' ? "Player wins!" :
+        status === 'dealer_won' ? "Dealer wins." :
+            "It's a tie.";
+
+    res.json({
+        message: resultMessage,
+        status: status,
         player: {
             hand: blackjack.playerHand,
             score: blackjack.playerScore,
+        },
+        dealer: {
+            hand: blackjack.dealerHand,
+            score: blackjack.dealerScore,
         }
     });
 });
+
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
